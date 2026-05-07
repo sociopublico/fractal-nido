953f9801295adf27ed5607abc1fee42512484aaf
Agus <m.agustina.nahas@gmail.com>
2026-05-07 10:29:42 -0300

    text fixces

--- src/components/DonutSection.jsx
+++ src/components/DonutSection.jsx
@@ -6,7 +6,7 @@ export default function DonutSection() {
       <div className="bg-[#0030870D] md:pt-24 pt-12">
         <div className="px-6 md:px-12">
           <h2 className="mx-auto mb-12 max-w-xl text-center md:text-4xl text-3xl font-medium text-black">
-            No todas las dimensiones pesan lo mismo en el indice.
+            No todas las dimensiones pesan lo mismo en el índice.
           </h2>
           <div className="mx-auto flex max-w-7xl flex-col justify-center gap-2 md:flex-row">
             <div className="w-full px-4 text-center md:w-1/3 border-b border-navy/10 md:border-b-0 pb-4 md:pb-0">
